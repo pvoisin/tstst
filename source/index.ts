afa5b4b0099a8ec83a952011b992c41855df0c9c
Pierre Voisin <pierre.voisin@gmail.com>
2019-05-29 02:14:42 -0400

    SQUASH: logic module

--- source/index.ts
+++ source/index.ts
@@ -3,5 +3,5 @@ export * from "./string.utility";
 export * from "./collection.utility";
 export * from "./date.utility";
 export * from "./identifiable";
-export * from "./logic.utility";
 export * from "./test.utility";
+export * from "./logic";
