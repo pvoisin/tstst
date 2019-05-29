4d75c928c18f54f716960912329759712b2ea5b3
Pierre Voisin <pierre.voisin@gmail.com>
2019-05-29 02:15:19 -0400

    Introduce `Identifiable`

--- source/index.ts
+++ source/index.ts
@@ -2,6 +2,5 @@ export * from "./utility";
 export * from "./string.utility";
 export * from "./collection.utility";
 export * from "./date.utility";
-export * from "./identifiable";
 export * from "./test.utility";
 export * from "./logic";
